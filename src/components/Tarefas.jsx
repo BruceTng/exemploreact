77565bd3c9c24c2f3d01dce8b9b2b51e235e8f13
Bruce Li <bruceetng@gmail.com>
2026-09-03 09:40:53 -0300

    feat:Adicionando Tailwindcss

--- src/components/Tarefas.jsx
+++ src/components/Tarefas.jsx
@@ -41,33 +41,32 @@ const Tarefas = () => {
 
     return (
     <>
-      <div className="todo-container">
-        <h2>Minha Lista de Tarefas</h2>
+      <div className="max-w-md mx-auto mt-10 p-6 bg-gray-400 rounded-2xl shadow-2xl border-2 border-gray-600">
+        <h2 className="text-2xl font-bold mb-6 text-center">Minha Lista de Tarefas</h2>
 
-        <form onSubmit={adicionarTarefas} className="todo-form">
+        <form onSubmit={adicionarTarefas} className="flex gap-2 mb-6">
           <input
             type="text"
             value={campo}
             onChange={(e) => setCampo(e.target.value)}
              placeholder="Digite uma nova tarefa..."
-            className="todo-input"
+            className="flex-1 px-4 border-gray-500 border rounded-lg focus:outline-none"
            />
-           <button type="submit" className="btn-adicionar">
+            <button type="submit" className="bg-gray-600 hover:bg-green-900 font-medium px-5 py-2 rounded-2xl transition-colors cursor-pointer">
              Adicionar
            </button>
         </form>
 
-         <ul className="todo-lista">
+         <ul className="space-y-3">
            {tarefas.map((tarefa) => (
-            <li key={tarefa.id} className="todo-item">
-               <span>{tarefa.text}</span>
+            <li key={tarefa.id} className="flex items-center justify-between p-3 bg-gray-300 border border-gray-600 rounded-lg shadow-xl hover:bg-gray-200 transition-colors">
+               <span className="text-shadow-black mr-2">{tarefa.text}</span>
                {/* arrow function (função seta) que encapsula a execução de outra função. 
           Ela garante que removerTarefa só seja executada quando o evento acontecer (como um clique de botão), 
-          e não assim que a página carregar.
-          */}
+          e não assim que a página carregar.*/}
+
                <button onClick={() => removerTarefa(tarefa.id)}
-                 className="btn-delete"
-              >
+                 className="bg-gray-500 font-medium hover:bg-red-800 px-3 py-1 rounded-2xl transition-colors cursor-pointer">
                 Excluir
                </button>
              </li>
